74a0859fdd0f6fb905606eb1dfb1ef03c5b42416
ligw <ligw@cq-gy.com>
2016-05-26 16:33:47 +0800

    完成了搜索功能

--- src/main/java/com/cqgy/park/web/CardInController.java
+++ src/main/java/com/cqgy/park/web/CardInController.java
@@ -3,6 +3,8 @@ package com.cqgy.park.web;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -25,11 +27,40 @@ public class CardInController {
 	@Autowired
 	JdbcTemplate jdbcTemplate;
 	@RequestMapping(value="/cardin/cardinlist.do",method=RequestMethod.GET)
-	public String list(Long page,Long pageSize, String orderby,HttpServletRequest request,Model model){
+	public String list(Long page,Long pageSize,String clause, String orderby,HttpServletRequest request,Model model){
+		System.out.println(clause);
+		String oclause=clause;
 		if (pageSize==null) {
 			pageSize=(long) 10;
 		}
-		String countsql="select count(*) count from info_card_in";
+		if (!Strings.isNullOrEmpty(clause)) {
+			Pattern pattern = Pattern.compile("[0-9]*");
+			Matcher isNum = pattern.matcher(clause.substring(clause.lastIndexOf("=")+1));
+			if(clause.contains("accept_emp_name")){
+				clause="accept_emp_name like '"+clause.substring(clause.lastIndexOf("=")+1)+"%'";
+			}else if(clause.contains("card_type")){
+				String cardType=clause.substring(clause.lastIndexOf("=")+1);
+				if (cardType.equals("月卡")) {
+					clause="card_type=1";
+				}else if(cardType.equals("免费卡")){
+					clause="card_type=2";
+				}else if(cardType.equals("储蓄卡")){
+					clause="card_type=3";
+				}
+			}else if(clause.contains("plate")){
+				clause="plate like '%"+clause.substring(clause.lastIndexOf("=")+1)+"%'";
+			}else{
+				if (!isNum.matches()) {
+					clause=clause.substring(0, clause.lastIndexOf("=")+1)+"null";
+				}
+			}
+		}
+		String countselect="select count(*) count from info_card_in";
+		String countclause="";
+		if (!Strings.isNullOrEmpty(clause)) {
+			countclause += " where "+clause;
+		}
+		String countsql=countselect+countclause;
 		Long count = (Long)jdbcTemplate.queryForList(countsql).get(0).get("count");
 		long pageMax;
 		if (count%pageSize==0) {
@@ -57,12 +88,15 @@ public class CardInController {
 		String limit=" limit "+pageStart+","+pageSize;
 		String where = "";
 		String sql;
+
+		
+		if(!Strings.isNullOrEmpty(clause)){
+			where += " where "+clause;
+		}
 		if(!Strings.isNullOrEmpty(orderby)){
 			where += " order by "+orderby;
-			sql=select+where+limit;
-		}else{
-			sql=select+limit;
 		}
+		sql=select+where+limit;
 		System.out.println(sql);
 		//String sql="select * from info_card_in";
 		List<InfoCardIn> cardIns = cardInService.getCardIns(sql);
@@ -76,6 +110,7 @@ public class CardInController {
 		session.setAttribute("nextpage", nextPage);
 		session.setAttribute("maxpage", pageMax);
 		session.setAttribute("orderby", orderby);
+		session.setAttribute("clause", oclause);
 		String forword="/cardin/cardinlist";
 		return forword;
 	}
