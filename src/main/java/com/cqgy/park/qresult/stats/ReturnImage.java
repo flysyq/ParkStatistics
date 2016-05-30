bd6ff2784e20b2d5280ee4ca7a6499799b548e1e
flysyq <flysyq@163.com>
2016-05-30 17:48:01 +0800

    增加统计图形显示功能

---
+++ src/main/java/com/cqgy/park/qresult/stats/ReturnImage.java
@@ -0,0 +1,25 @@
+/**
+ * @作者 admin
+ * @时间 2016年5月30日 下午3:08:04
+ * @类名 ReturnImage.java
+ * @类描述 
+ * @修改记录
+ * 1、修改人 2016年5月30日 下午3:08:04
+ *   修改描述
+ */
+package com.cqgy.park.qresult.stats;
+
+public class ReturnImage {
+
+	private String image ;
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+}
